7d6d7f60083c5e97412fff14678c7efe0c5a3593
chl1860 <chenlong1988.love@163.com>
2014-11-18 01:12:35 +0800

    Add methods

---
+++ JsonSerializer/JsonSerializer.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Text;
+using System.Runtime.Serialization.Json;
+
+namespace Ame.Twitter.Ultility
+{
+    public class JsonSerializer<T>
+    {
+        public static T ToObject(string json)
+        {
+            var ser = new DataContractJsonSerializer(typeof(T));
+
+            using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(json)))
+            {
+                var result = (T)ser.ReadObject(stream);
+                return result;
+            }
+        }
+
+        public static string ToJsonString(T obj)
+        {
+            var ms = new MemoryStream();
+            var ser = new DataContractJsonSerializer(typeof(T));
+            ser.WriteObject(ms,obj);
+            byte[] json = ms.ToArray();
+            ms.Close();
+            return Encoding.UTF8.GetString(json,0,json.Length);
+        }
+    }
+}
